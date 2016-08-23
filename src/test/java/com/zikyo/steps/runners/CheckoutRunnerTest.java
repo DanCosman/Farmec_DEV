8c6088416b75bbf8821103e8f5171b2e67aabd96
dcosman <cosmandaniel@gmail.com>
2016-08-23 13:26:04 +0300

    runnerele inapoi

--- src/test/java/com/zikyo/steps/runners/CheckoutRunnerTest.java
+++ src/test/java/com/zikyo/steps/runners/CheckoutRunnerTest.java
@@ -8,11 +8,11 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
 monochrome = true,
-        plugin =  {"rerun", "html:target/cucumber","json:target/jsonReports/LoginTest.json"},
+        plugin =  {"rerun", "html:target/cucumber","json:target/jsonReports/CheckoutTest.json"},
         glue = {
         "com.zikyo"
         },
 
-        features = "src/test/resources/features/login.feature")
-public class LoginRunnerTest {
+        features = "src/test/resources/features/checkout.feature")
+public class CheckoutRunnerTest {
 }
