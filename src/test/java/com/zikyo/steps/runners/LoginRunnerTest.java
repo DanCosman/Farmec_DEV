5bde222afb8d17ac8205e5c22eb23878fed74d03
dcosman <cosmandaniel@gmail.com>
2016-08-23 11:49:25 +0300

    changed runner AGAIN & Again

--- src/test/java/com/zikyo/steps/runners/LoginRunnerTest.java
+++ src/test/java/com/zikyo/steps/runners/LoginRunnerTest.java
@@ -5,6 +5,14 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (features = "src/test/resources/features/")
+
+@CucumberOptions(
+monochrome = true,
+        plugin =  {"rerun", "html:target/cucumber","json:target/jsonReports/CheckoutTest.json"},
+        glue = {
+        "com.zikyo"
+        },
+
+        features = "src/test/resources/features/")
 public class LoginRunnerTest {
 }
