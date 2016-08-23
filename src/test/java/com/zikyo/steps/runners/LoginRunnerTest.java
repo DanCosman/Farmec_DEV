c6efc212b0ae0c80a935a4bc5412b4b5d11907f4
dcosman <cosmandaniel@gmail.com>
2016-08-23 11:14:55 +0300

    lasat un singur runner

--- src/test/java/com/zikyo/steps/runners/LoginRunnerTest.java
+++ src/test/java/com/zikyo/steps/runners/LoginRunnerTest.java
@@ -8,12 +8,15 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
         monochrome = true,
-        plugin =  {"rerun", "html:target/cucumber","json:target/jsonReports/LoginTest.json"},
+        plugin =  {"rerun", "html:target/cucumber","json:target/jsonReports/LoginTest.json",
+        "json:target/jsonReports/RegisterTest.json", "json:target/jsonReports/CheckoutTest.json"},
         glue = {
                 "com.zikyo"
         },
         features = {
-                "src/test/resources/features/login.feature"
+                "src/test/resources/features/login.feature",
+                "src/test/resources/features/register.feature",
+                "src/test/resources/features/checkout.feature"
         }
 )
 public class LoginRunnerTest {
