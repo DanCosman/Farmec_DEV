9805d7366989032ef5711343d5df6433d0492d1a
dcosman <cosmandaniel@gmail.com>
2016-08-23 13:44:57 +0300

    pus inapoi if / else

--- src/test/java/com/zikyo/steps/loginSteps/LoginSteps.java
+++ src/test/java/com/zikyo/steps/loginSteps/LoginSteps.java
@@ -1,7 +1,10 @@
 package com.zikyo.steps.loginSteps;
 
+import com.sdl.selenium.web.WebLocator;
 import com.zikyo.utils.Farmec.LoginPage;
 import cucumber.api.java.en.When;
+
+import static com.sdl.selenium.utils.config.WebDriverConfig.getDriver;
 
 /**
  * Created by Sica-sama on 7/20/2016.
@@ -11,6 +14,15 @@ public class LoginSteps {
 
     @When("^I login with \"([^\"]*)\"/\"([^\"]*)\"$")
     public void iLoginWith(String email, String pass) throws Throwable {
-        loginPage.doLogin(email, pass);
+        WebLocator elementT = new WebLocator().setText("Utilizator nou");
+        if (elementT.isVisible())
+        {
+            getDriver().navigate().to("https://dev2.farmec.ro/autentificare.html");
+            loginPage.doLogin(email, pass);
+        }
+        else {
+            getDriver().navigate().to("https://dev2.farmec.ro/autentificare.html?action=logout");
+            loginPage.doLogin(email, pass);
+        }
     }
 }
